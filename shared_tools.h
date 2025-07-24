d24b9a91f6a32021227b8fc501153f6309b2c8dd
murrayld <landondmurray@yahoo.com>
2025-07-23 23:22:57 -0700

    change literal to unsigned to prevent conversion warnings

--- shared_tools.h
+++ shared_tools.h
@@ -1,2 +1,11 @@
 #ifndef _SHARED_TOOLS_H
-#def _SHARED_TOOLS_H
+#define _SHARED_TOOLS_H
+
+#define BIT(x) (0x01U << x)
+#define MASK(x) ((0x01U << (x + 1)) - 1)
+#define TRUE 1
+#define FALSE 0
+#define ENABLE 1
+#define DISABLE 0
+
+#endif
