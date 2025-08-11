03af32fd84101132b8c8bd91961458b052145e56
murrayld <landondmurray@yahoo.com>
2025-08-10 17:18:35 -0700

    read_memrange updates. Function working now

--- debug.c
+++ debug.c
@@ -1,6 +1,7 @@
 #include "debug.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "usart.h"
 
 void memdump(uint32_t addr, char buff[], uint32_t buffsize)
@@ -24,6 +25,18 @@ void memdump_range(uint32_t addr_start, uint32_t addr_stop, char buff[], uint32_
 	for (uint32_t* addr_ptr = (uint32_t*)addr_start; (uint32_t)addr_ptr <= addr_stop; addr_ptr += 4)
 	{
 		memdump((uint32_t)addr_ptr, buff, buffsize);
-		usart_transmit_bytes(USART_DEBUG, buff, 57);
+		usart_transmit_bytes(USART_DEBUG, buff, 57U, '\0'); // snprintf null terminates output
 	}
+}
+
+void read_memrange(char buff[], uint32_t buffsize)
+{
+	uint32_t addr_start = 0, addrt_stop = 0; //used to store addresses.
+	// requests range of memaddresses from user
+	usart_transmit_bytes(USART_DEBUG, "enter address in format: 0xstart 0xend\n\r\0", 100, '\0');
+	usart_read_with_echo(USART_DEBUG, buff, buffsize);
+	sscanf(buff, "0x%08lx 0x%08lx", &addr_start, &addrt_stop);
+	memdump_range(addr_start, addrt_stop, buff, buffsize);
+	usart_transmit_bytes(USART_DEBUG, "\n\r\0", 50, '\0');
+	usart_transmit_bytes(USART_DEBUG, buff, buffsize, '\0');
 }
--- debug.h
+++ debug.h
@@ -1,8 +1,9 @@
 #ifndef _DEBUG_H
 #include <stdint.h>
 #define _DEBUG_H
-#define MEM_ADDR_MAX  0x0000ffff
+#define MEM_ADDR_MAX  0xD0000000
 
 void memdump(uint32_t addr, char buff[], uint32_t buffsize);
 void memdump_range(uint32_t addr_start, uint32_t addr_stop, char buff[], uint32_t buffsize);
+void read_memrange(char buff[], uint32_t buffsize);
 #endif
