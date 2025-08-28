7123f253686895c6a04682465bcf2301831d701e
murrayld <landondmurray@yahoo.com>
2025-08-28 00:27:33 -0700

    syscfg.c syscfg.h

---
+++ syscfg.h
@@ -0,0 +1,13 @@
+#ifndef _SYSCFG_H
+#def _SYSCFG_H
+
+#include <stdint.h>
+
+// define syscfg struct
+// 08/27/25. Note there seem to be some discrepancies between the syscfg register map
+// and the STM32 programmer tool. Which shows does not show some of the registers
+// outlined in the reference manual.
+
+struct SYSCFG {volatile uint32_t RSVD0, PMCR, EXTICR1, EXTICR2, EXTICR3, EXTICR4,
+RSVD1[3] ,CCCSR, CCVR, CCCR, RSVD1[62], PKGR, RSVD2[118], UR0, RSVD3,
+UR2, UR3, UR4, UR5, UR6, UR7, RSVD[3], UR11, UR12, UR13, UR14, UR15, UR16, UR17};
