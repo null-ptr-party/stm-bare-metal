08c4c139b1aa2cd18eec3028e1d469ce6ef14971
murrayld <landondmurray@yahoo.com>
2025-09-02 23:34:54 -0700

    fix syntax errors

--- syscfg.h
+++ syscfg.h
@@ -1,5 +1,5 @@
 #ifndef _SYSCFG_H
-#def _SYSCFG_H
+#define _SYSCFG_H
 
 #include <stdint.h>
 
@@ -8,9 +8,11 @@
 // and the STM32 programmer tool. Which shows does not show some of the registers
 // outlined in the reference manual.
 
-struct SYSCFG {volatile uint32_t RSVD0, PMCR, EXTICR1, EXTICR2, EXTICR3, EXTICR4,
-RSVD1[2] ,CCCSR, CCVR, CCCR, RSVD1[62], PKGR, RSVD2[118], UR0, RSVD3,
-UR2, UR3, UR4, UR5, UR6, UR7, RSVD[3], UR11, UR12, UR13, UR14, UR15, UR16, UR17};
+struct SYSCFG {
+	volatile uint32_t RSVD0, PMCR, EXTICR1, EXTICR2, EXTICR3, EXTICR4,
+	RSVD1[2], CCCSR, CCVR, CCCR, RSVD1[62], PKGR, RSVD2[118], UR0, RSVD3,
+	UR2, UR3, UR4, UR5, UR6, UR7, RSVD[3], UR11, UR12, UR13, UR14, UR15, UR16, UR17;
+};
 
 // define syscfg struct
 #define ((struct SYSCFG *)0x58000400)
