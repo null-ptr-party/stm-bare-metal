9be05d89e0cbfdbe29f846b7b422f2ad256c759b
murrayld <landondmurray@yahoo.com>
2025-09-06 14:30:21 -0700

    working interrupt on blue hat button

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include "shared_tools.h"
 #include "pwr.h"
 #include "debug.h"
+#include "exti.h"
+#include "syscfg.h"
+#include "nvic.h"
 
 // below are defined in linker script. Hence extern
 extern void _estack(void);
@@ -18,21 +21,27 @@ void _reset(void);
 void cnt_down(volatile uint32_t cnt);
 void systick_handler(void);
 void wait_ms(uint32_t ms);
+void exti13_iqr(void);
 
 // Globals
 static volatile uint32_t s_ticks = 0;
+char inbuff[256] = { 0 };
 
 // Create section for NVIC vector table
 __attribute__((section(".vectors"))) void(*const table[16 + 163])(void) = {
 	// set _estack, reset to first elements of table.
-	_estack, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, systick_handler // systick is the 16th word
+	_estack, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, systick_handler, // systick is the 16th word
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, exti13_iqr
 };
 
 int main(void)
 {
-	enable_cfg();
-	//cfg_pwr_input();
-	//blk_til_actvos_rdy();
+	// ====================== Configure power and voltage scaling====================
+	cfg_pwr_input();
+	// change VOS scaling to 2
+	set_core_scl(VOS_SCALE_2);
+	
 	// =======================Configure clocks and pllss=============================
 	// start HSE
 	start_hse(TRUE);
@@ -42,11 +51,11 @@ int main(void)
 		;
 	}
 
-
-	// setup pll1. 150 Mhz target. PLL div_fctrs cannot be 0.
+	while ((PWR->PWR_D3CR & BIT(13)) == 0);
+	// setup pll1. 250 Mhz target. PLL div_fctrs cannot be 0.
 	struct pll_config pll1config = { .pll_prscl = 1U, .pll_src = PLL_SRC_HSE, .divp_en = TRUE, .divq_en = FALSE,
 		.divr_en = FALSE, .pll_in_rng = PLL_IN_RNG_FOUR_EIGHT, .vco_rng = VCO_RNG_MED, .frac_en = FALSE, .div_fctr_p = 1U,
-		.div_fctr_q = 1U, .div_fctr_r = 1U, .pll_mult = 30U};
+		.div_fctr_q = 1U, .div_fctr_r = 1U, .pll_mult = 50U};
 
 	cfg_pll(&pll1config, PLL1);
 	start_pll(PLL1);
@@ -56,6 +65,7 @@ int main(void)
 		; // block until pll ready
 	}
 
+	set_sys_clk(SYSCLK_SRC_PLL1);
 	// setup pll2 384,000,000 hz target. 76.8 PLL multiplier. 11x multiplier 6525 fractional multiplier. Must be in range
 	// 192-836 Mhz
 	// this is mainly driven by the desire to have an integer multiple of standard baud rates.
@@ -71,13 +81,6 @@ int main(void)
 		; // block until pll ready
 	}
 
-	// set VOS scaling for VOS2. Currently not working. Work around is to
-	// use valid levels for VOS3
-	//set_core_scl(VOS_SCALE_2);
-	//blk_til_vos_rdy();
-	//setup sysclk
-	set_sys_clk(SYSCLK_SRC_PLL1);
-
 	// setup krnl clocks
 	struct krnl_clk_cfg krnl_cfg = { .d2_usart234578 = 1U };
 	cfg_krnl_clks(&krnl_cfg);
@@ -86,7 +89,7 @@ int main(void)
 
 	// =======================Setup Systick ==============================
 	struct systick_setup settings = { .ctr_enbl = CTR_ENABLE,
-	.excpt_enbl = EXCPT_ENBL, .clksrc = CLKSRC_PRC, .rld_val = 0x249f0 };
+	.excpt_enbl = EXCPT_ENBL, .clksrc = CLKSRC_PRC, .rld_val = 0x3d090 };
 
 	struct systick_setup* stg_ptr = &settings;
 
@@ -122,9 +125,16 @@ int main(void)
 	set_mco_src(MCO2, MCO2_SRC_PLL2P);
 	set_mco_prsc(MCO1, 1);
 
-
-
 	// =======================End GPIO setup =============================
+	// ======================= Interrupt Setup ===========================
+	enable_cfg(); // enable syscfg in rcc
+	set_gpio_mode(GPIOC, 13U, GPIO_MODE_INPUT);
+	unmask_interrupt(40U);
+	cfg_exti_interrupt(EXTI_PORT_C, 13); // configure interrupt for pin13
+	cfg_interrupt(13, EDGETYPE_RISING); // unmask interrupt and set to rising edge.
+	
+	// set up interrupt for debug PC13
+	// set exti13 for port C
 
 	// ======================= USART Setup ===============================
 	// clock usart
@@ -134,7 +144,6 @@ int main(void)
 	struct usart_cfg usart_cfg = { .word_len = DATABITS8, .oversmpl_mthd = OVERSAMPLE16, .baud = 0x14DUL }; // target baud 0x1800
 	setup_usart(USART3, &usart_cfg);
 	//enable_tx(USART3);
-	char inbuff[256] = { 0 };
 	//memdump_range(0x20000090U, 0x200000f0U, inbuff, 256U);
 	
 	while (1)
@@ -145,7 +154,6 @@ int main(void)
 		reset_gpio_output(GPIOB, 14U);
 		//reset_gpio_output(GPIOB, 8U);
 		//usart_read_with_echo(USART_DEBUG, inbuff, 256U);
-		read_memrange(inbuff, 256);
 		//usart_read_bytes(USART3, inbuff, 256, '\n');
 		//usart_read_bytes(USART3, inbuff, 256, '\r');
 		wait_ms(1000);
@@ -189,4 +197,10 @@ void wait_ms(uint32_t ms)
 	{
 		;
 	}
+}
+
+void exti13_iqr(void)
+{
+	read_memrange(inbuff, 256);
+	EXTI->CPUPR1 |= BIT(13); // clear pending bit.
 }
