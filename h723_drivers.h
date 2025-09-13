a36f4947f9c01076a1e63876f5bf70b0aa837680
murrayld <landondmurray@yahoo.com>
2025-09-13 00:29:51 -0700

    add driver header to reduce individual includes in main

---
+++ h723_drivers.h
@@ -0,0 +1,16 @@
+#ifndef _H723_DRIVERS_H
+#define _H723_DRIVERS_H
+
+// include all driver headers
+#include "debug.h"
+#include "exti.h"
+#include "gpio.h"
+#include "nvic.h"
+#include "pwr.h"
+#include "rcc.h"
+#include "syscfg.h"
+#include "systick.h"
+#include "tim.h"
+#include "usart.h"
+
+#endif
