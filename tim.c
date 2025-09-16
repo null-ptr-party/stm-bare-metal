b1d0952e9ee6398143f7dbeabb6282c7993061d0
murrayld <landondmurray@yahoo.com>
2025-09-15 21:02:43 -0700

    add driver functions for advanced timer

--- tim.c
+++ tim.c
@@ -1,1 +1,57 @@
 #include "tim.h"
+
+// Advanced timer functions.
+// Macros should be used for channels and mode selection
+void set_atim_capmode(struct adv_tim* atim_ptr, uint8_t ch, uint8_t mode)
+{ // set atim capture mode.
+	uint32_t mode_shifted = (mode << 8*ch); // mode with shift applied for ch.
+
+	if (ch <= 1)
+	{
+		atim_ptr->CCMR1 = ((atim_ptr->CCMR1 & ~(mode_shifted)) | (mode_shifted));
+	}
+	else if (ch <= 3)
+	{
+		atim_ptr->CCMR2 = ((atim_ptr->CCMR2 & ~(mode_shifted)) | (mode_shifted));
+	}
+}
+
+void set_atim_filter(struct adv_tim* atim_ptr, uint8_t ch, uint8_t fset)
+{ // sets atim filter setting. Must use macro for channel.
+	uint32_t fset_shifted = (fset << (ch*8 + 4); // mode with shift applied for ch.
+
+	if (ch <= 1)
+	{
+		atim_ptr->CCMR1 = ((atim_ptr->CCMR1 & ~(fset_shifted)) | (fset_shifted));
+	}
+	else if (ch <= 3)
+	{
+		atim_ptr->CCMR2 = ((atim_ptr->CCMR2 & ~(fset_shifted)) | (fset_shifted));
+	}
+}
+
+void set_atim_prescl(struct adv_tim* atim_ptr, uint8_t ch, uint8_t prscl)
+{ // set atim prescaler.
+	uint32_t prscl_shifted = (prscl << (8*ch + 2); // mode with shift applied for ch.
+
+	if (ch <= 1)
+	{
+		atim_ptr->CCMR1 = ((atim_ptr->CCMR1 & ~(prscl_shifted)) | (prscl_shifted));
+	}
+	else if (ch <= 3)
+	{
+		atim_ptr->CCMR2 = ((atim_ptr->CCMR2 & ~(prscl_shifted)) | (prscl_shifted));
+	}
+
+}
+
+// enable capture compare and set polarity. Recommend using macro
+void enable_atim_ch(struct adv_tim* atim_ptr, uint8_t ch)
+{ // Note macros should be used.
+	atim_ptr->CCER |= BIT((2*ch));
+}
+
+void set_atim_polarity(struct adv_tim* atim_ptr, uint8_t ch, uint8_t polarity)
+{ // Note macros should be used.
+	atim_ptr->CCER |= polarity*BIT((2*ch + 1));
+}
--- tim.h
+++ tim.h
@@ -14,4 +14,45 @@ struct adv_tim {
 #define TIM1 ((struct adv_tim*)0x40010000)
 #define TIM2 ((struct adv_tim*)0x40010400)
 
+// advances timer macros
+// mode macros
+#define CC_MODE_OUTPUT 0U
+#define CC_MODE_INPUT_TI1 1U
+#define CC_MODE_INPUT_TI2 2U
+#define CC_MODE_INPUT_TRC 3U
+
+// channel macros
+#define ATIM_CH1 0U
+#define ATIM_CH2 1U
+
+// capture compare ch macros
+#define TIM_CAPCOMP_CH1 0U
+#define TIM_CAPCOMP_CH1COMP 1U
+#define TIM_CAPCOMP_CH2 2U
+#define TIM_CAPCOMP_CH2COMP 3U
+#define TIM_CAPCOMP_CH3_EN 4U
+#define TIM_CAPCOMP_CH3COMP 5U
+#define TIM_CAPCOMP_CH4 6U
+#define TIM_CAPCOMP_CH4COMP 7U
+#define TIM_CAPCOMP_CH5 8U
+#define TIM_CAPCOMP_CH5COMP 9U
+#define TIM_CAPCOMP_CH6 10U
+#define TIM_CAPCOMP_CH6COMP 11U
+
+// polarity macro
+#define TIM_POLARITY_NORMAL 0
+#define TIM_POLARITY_INV 1
+
+// Advanced timer functions. Macros should be used for channels
+// set capture mode of advanced timer
+void set_atim_capmode(struct adv_tim* atim_ptr, uint8_t ch, uint8_t mode);
+// apply filter setting for timer
+void set_atim_filter(struct adv_tim* atim_ptr, uint8_t ch, uint8_t fset);
+// set capture signal prescaler
+void set_atim_prescl(struct adv_tim* atim_ptr, uint8_t ch, uint8_t prscl);
+// enable capture compare. Recommend using macro
+void enable_atim_ch(struct adv_tim* atim_ptr, uint8_t ch);
+// set timer polarity
+void set_atim_polarity(struct adv_tim* atim_ptr, uint8_t ch, uint8_t polarity)
+
 #endif
