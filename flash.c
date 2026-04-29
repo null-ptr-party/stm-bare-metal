2dc58561c18dd68b94d3712138c79209ff612d8f
murrayld <landondmurray@yahoo.com>
2026-04-28 21:05:18 -0700

    define set wait states and set prog delay functions

--- flash.c
+++ flash.c
@@ -1,8 +1,18 @@
-/*
- * flash.c
- *
- *  Created on: Apr 28, 2026
- *      Author: lando
- */
+#include <stdint.h>
+#include "flash.h"
+#include "shared_tools.h"
+
+// Set number of wait states
+void set_wait_states(uint8_t num_states)
+{
+	uint32_t word = num_states & MASK(3); // ensure correct range
+	FLASH->ACR = (FLASH->ACR & ~MASK(3)) | word;
+}
+// set write frequency
+void set_prog_delay(uint8_t delay)
+{
+	uint32_t word = ((delay & MASK(1)) << 4U);
+	FLASH->ACR = ((FLASH->ACR & ~(MASK(1) << 4U)) | word);
+}
 
 
